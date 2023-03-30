4b17da06d6c79cdc16f32c7d386b4a638feed0ed
Vladimir Stanislavski <stanislavskivladimir@gmail.com>
2023-03-30 16:18:44 +0300

    Introduce JUnit5 Test

---
+++ test/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,10 @@
+package webapp.storage;
+
+class ArrayStorageTest extends AbstractArrayStorageTest {
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
+
+
+
+}
---
+++ test/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,11 @@
+package webapp.storage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortedArrayStorageTest extends AbstractArrayStorageTest {
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
+
+    }
+
+}
